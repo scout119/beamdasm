df2a757a1298205a8a3b1809e42dc727cc6dae70
Valentin Ivanov <valen.ivanov@gmail.com>
2018-04-04 17:30:18 -0400

    Cleaned up beam file search

--- src/beamFilesProvider.ts
+++ src/beamFilesProvider.ts
@@ -9,14 +9,19 @@ export default class BeamFilesProvider implements vscode.TreeDataProvider<vscode
 
   onDidChangeTreeData?: vscode.Event<vscode.TreeItem> | undefined;
 
+  dev: BeamVirtualFolder = new BeamVirtualFolder('dev');
+  prod: BeamVirtualFolder = new BeamVirtualFolder('prod');
+  deps: BeamVirtualFolder = new BeamVirtualFolder('deps');
+
+
   constructor(private workspaceRoot: string | undefined) {
 
-    if( workspaceRoot && fs.existsSync(workspaceRoot) ){
+    if (workspaceRoot && fs.existsSync(workspaceRoot)) {
       //Watcher is not 100% consistent on all platforms 
-      fs.watch(workspaceRoot, {recursive: true}, (event: string, filename: string | Buffer) => {
-        let file = ( filename instanceof Buffer ) ? filename.toString() : filename;
+      fs.watch(workspaceRoot, { recursive: true }, (event: string, filename: string | Buffer) => {
+        let file = (filename instanceof Buffer) ? filename.toString() : filename;
 
-        if( path.extname(file) === ".beam" ){
+        if (path.extname(file) === ".beam") {
           console.log(`${event} happend to ${filename}`);
         }
         return undefined;
@@ -30,89 +35,107 @@ export default class BeamFilesProvider implements vscode.TreeDataProvider<vscode
   }
 
   getChildren(element?: vscode.TreeItem): vscode.ProviderResult<vscode.TreeItem[]> {
+
     if (!this.workspaceRoot) {
       vscode.window.showInformationMessage('No beam files in empty workspace');
       return Promise.resolve([]);
     }
 
+    let provider = this;
+    let root = this.workspaceRoot;
 
-    let beamFiles: any[] = [];
-    const isDirectory = (source: string) => fs.lstatSync(source).isDirectory();
+    return new Promise(resolve => {
 
-    const getBeamFiles = (source: string, acc: any[]) => {
-      let items = fs.readdirSync(source);
-      items.forEach((value: string) => {
-        try {
-          let fullPath = path.join(source, value);
-          if (isDirectory(fullPath)) {
-            getBeamFiles(fullPath, acc);
-          } else {
-            if (path.extname(fullPath) === ".beam") {
-              acc.push({ name: value, location: fullPath });
-            }
-          }
-        } catch (ex) {
-        }
-        finally {
-        }
-      });
-    };
+      let final: vscode.TreeItem[] | undefined;
 
-    getBeamFiles(this.workspaceRoot, beamFiles);
-
-    if (!element) {
-      return new Promise(
-        resolve => {
-          resolve(
-            beamFiles.map<BeamFileItem>(
-              (item: any, index: number, array: any[]) => {
-                return new BeamFileItem(item.name,
-                  item.location
-                );
+      if (!element) {        
+        let beamFiles: any[] = [];
+        const isDirectory = (source: string) => fs.lstatSync(source).isDirectory();
+        const getBeamFiles = (source: string, acc: any[]) => {
+          let items = fs.readdirSync(source);
+          items.forEach((value: string) => {
+            try {
+              let fullPath = path.join(source, value);
+              if (isDirectory(fullPath)) {
+                getBeamFiles(fullPath, acc);
+              } else {
+                if (path.extname(fullPath) === ".beam") {
+                  if (fullPath.search('_build\\\\prod') !== -1) {
+                    this.prod.items.push(new BeamFileItem(value, fullPath));
+                  }
+                  else if (fullPath.search('_build\\\\dev') !== -1) {
+                    this.dev.items.push(new BeamFileItem(value, fullPath));
+                  }
+                  else if (fullPath.search('deps') !== -1) {
+                    this.deps.items.push(new BeamFileItem(value, fullPath));
+                  }
+                  else {
+                    acc.push({ name: value, location: fullPath });
+                  }
+                }
               }
-            )
-          );
-        }
-      );
-    } else if (element instanceof BeamFileItem) {
-      if (fs.existsSync(element.filePath)) {
-        let bm = BeamFile.fromFile(element.filePath);
-        if ('impt' in bm._chunks) {
-          let item = new BeamChunkItem("ImptT");
-          item.iconPath = {
-            light: path.join(__filename, '..','..', 'resources', 'light', 'atom.svg'),
-            dark: path.join(__filename, '..', '..', 'resources', 'dark', 'atom.svg')
-          };
+            } catch (ex) {
+            }
+            finally {
+            }
+          });
+        };
+        getBeamFiles(root, beamFiles);
+  
 
-          return Promise.resolve(
-            [
-              item
-            ]
-          );
+        let top: vscode.TreeItem[] = [provider.dev, provider.prod, provider.deps];
+        final = top.concat(
+          beamFiles.map<vscode.TreeItem>(
+            (item: any, index: number, array: any[]) => {
+              return new BeamFileItem(item.name,
+                item.location
+              );
+            }
+          ));
+      } else if (element instanceof BeamFileItem) {
+        if (fs.existsSync(element.filePath)) {
+          let bm = BeamFile.fromFile(element.filePath);
+          if ('impt' in bm._chunks) {
+            let item = new BeamChunkItem("ImptT");
+            item.iconPath = {
+              light: path.join(__filename, '..', '..', 'resources', 'light', 'atom.svg'),
+              dark: path.join(__filename, '..', '..', 'resources', 'dark', 'atom.svg')
+            };
+
+            final = [item];
+          }
         }
+      } else if (element instanceof BeamVirtualFolder) {
+        final = element.items;
       }
-    }
 
-    return Promise.resolve([]);
+      return resolve(final);
+    });
   }
 }
 
+class BeamVirtualFolder extends vscode.TreeItem {
+  constructor(public readonly label: string) {
+    super(label, vscode.TreeItemCollapsibleState.Expanded);
+  }
 
+  items: vscode.TreeItem[] = [];
+}
 class BeamFileItem extends vscode.TreeItem {
 
-  elixir:boolean = false;
+  elixir: boolean = false;
 
-  constructor(public readonly label: string,    
+  constructor(public readonly label: string,
     public readonly filePath: string
   ) {
     super(label, vscode.TreeItemCollapsibleState.Collapsed);
 
-    if( label.startsWith('Elixir.') ){
-      this.elixir = true;    
+    if (label.startsWith('Elixir.')) {
+      this.elixir = true;
     }
 
     this.iconPath = {
-      light: path.join(__filename, '..','..', 'resources', this.elixir ? 'elixir.svg' : 'erlang.svg'),
+      light: path.join(__filename, '..', '..', 'resources', this.elixir ? 'elixir.svg' : 'erlang.svg'),
       dark: path.join(__filename, '..', '..', 'resources', this.elixir ? 'elixir.svg' : 'erlang.svg')
     };
   }
@@ -127,11 +150,11 @@ class BeamFileItem extends vscode.TreeItem {
     command: "beamdasm.disassemble",
     arguments: [vscode.Uri.file(this.filePath), this],
     title: 'Open BEAM'
-  };  
+  };
 }
 
 class BeamChunkItem extends vscode.TreeItem {
-  constructor(public readonly label: string){
-    super(label,vscode.TreeItemCollapsibleState.None);
+  constructor(public readonly label: string) {
+    super(label, vscode.TreeItemCollapsibleState.None);
   }
 }
